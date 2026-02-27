f52f083c6dadbfc1852e9b341c2bdf0948eeb014
Nishant-Dave <nishant.nsbda@gmail.com>
2026-02-27 22:37:50 +0530

    bug fix 1

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
+from dashboard.views import dashboard_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dashboard_home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('expenses/', include('expenses.urls')),
