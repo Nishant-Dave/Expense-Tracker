b019ede4ae60044b80274c7e534c5d01097488dc
Nishant-Dave <nishant.nsbda@gmail.com>
2026-02-28 11:21:10 +0530

    ff bug fix 5

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# --- ADDED FOR VERCEL ---
+# This calculates the path to the 'src' folder and tells Python to look inside it
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# ------------------------
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# Vercel sometimes expects the WSGI application to be named 'app' instead of 'application'
+app = application
